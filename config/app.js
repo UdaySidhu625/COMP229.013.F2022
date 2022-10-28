1c2878545b45c4f492011d2d926c47e8fb507bf4
UdaySidhu625 <sidhuudaypartapsingh@gmail.com>
2022-10-28 17:37:10 -0400

    Assignment 2
    Assignment 2 Partially Complete

--- config/app.js
+++ config/app.js
@@ -1,13 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let bodyParser = require('body-parser');
+let session = require('express-session');
+let flash = require('connect-flash');
+let passport = require('passport');
+
+var app = express();
+
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: "sessionSecret"
+}));
 
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
+var businessConRouter = require('../routes/businessCon.router');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
@@ -20,9 +32,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
 
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/businessCon', businessConRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
